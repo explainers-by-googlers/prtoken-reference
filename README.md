11e90a8b4aba27f176e7f3d001759d754377cfe8
linxinan <linxinan@google.com>
2025-02-27 13:15:00 -0800

    Copy PRT reference from Github[1] to GOB
    The sources are downloaded from commit[2].
    
    [1] https://github.com/explainers-by-googlers/prtoken-reference
    [2] https://github.com/explainers-by-googlers/prtoken-reference/commit/2d1f9448fe08994049c297cdf681b006563fbb2a
    
    BUG=b/399706455
    Change-Id: If26487dc04d92ecae23d693767e433eff4da785f

---
+++ README.md
@@ -0,0 +1,22 @@
+# Probabilistic Reveal Tokens
+
+Probabilistic Reveal Tokens allow a client to limit the disclosure of a signal
+to a predetermined probability and prove adherence to this probability.
+This repository contains the
+[description of the protocol](protocol.md) as well as a reference
+implementation.
+
+## Overview of components
+
++ prtoken/
+  +   token.{h,cc,proto} - Implementation of the token itself.
+  +   issuer.{h,cc} - Token issuance
+  +   client.{h,cc} - Re-randomization of ciphertext
+  +   verifier.{h, cc} - Verification of token contents and reveal rate.
++  protocol.md - Description of the protocol
++  README.md - This file
+
+## How to compile the protocol source
+1. Install [Bazelisk](https://github.com/bazelbuild/bazelisk)
+2. Build the libraries: `bazelisk build prtoken:all`
+
---
+++ prtoken/verifier.h
@@ -0,0 +1,106 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+#ifndef PRTOKEN_VERIFIER_H_
+#define PRTOKEN_VERIFIER_H_
+
+#include <sys/types.h>
+
+#include <cstddef>
+#include <cstdint>
+#include <map>
+#include <memory>
+#include <string>
+#include <utility>
+#include <vector>
+
+#include "private_join_and_compute/crypto/big_num.h"
+#include "private_join_and_compute/crypto/context.h"
+#include "private_join_and_compute/crypto/elgamal.h"
+#include "private_join_and_compute/crypto/elgamal.pb.h"
+#include "prtoken/token.h"
+#include "prtoken/token.pb.h"
+#include "absl/status/status.h"
+#include "absl/status/statusor.h"
+#include "absl/strings/string_view.h"
+#include "absl/types/span.h"
+
+namespace prtoken {
+
+enum VerificationError {
+  VERIFICATION_ERROR_UNKNOWN,
+  VERIFICATION_ERROR_DECRYPT_FAILED,
+  VERIFICATION_ERROR_PARSE_FAILED,
+  VERIFICATION_ERROR_INVALID_HMAC
+};
+
+using ::private_join_and_compute::BigNum;
+using ::private_join_and_compute::Context;
+using ::private_join_and_compute::ElGamalPublicKey;
+using ::private_join_and_compute::ElGamalSecretKey;
+
+using VerificationReport = std::vector<std::pair<size_t /** index */,
+                                                  VerificationError>>;
+
+// Decrypts a probabilistic reveal token into a plaintext message.
+// Creates and holds its own context and ECGroup, which are NOT thread-safe.
+// Consider creating a single decrypter per thread.
+class Decrypter {
+ public:
+  Decrypter() = default;
+  void Init(const private_join_and_compute::ElGamalSecretKey& elgamal_secret_key);
+  absl::StatusOr<std::string> Decrypt(
+      const private_join_and_compute::ElGamalCiphertext& token) const;
+
+ private:
+  std::unique_ptr<private_join_and_compute::Context> blinders_context_;
+  std::unique_ptr<private_join_and_compute::ECGroup> ec_group_;
+  std::unique_ptr<private_join_and_compute::ElGamalDecrypter> decrypter_;
+};
+
+class Verifier {
+ public:
+  static absl::StatusOr<std::unique_ptr<Verifier>> Create(
+      const private_join_and_compute::ElGamalSecretKey& elgamal_secret_key,
+      absl::string_view hmac_secret);
+
+absl::Status DecryptTokens(
+    absl::Span<const private_join_and_compute::ElGamalCiphertext> tokens,
+    std::vector<proto::PlaintextToken>& messages,
+    std::vector<proto::VerificationErrorReport>& reports);
+
+  std::map<uint8_t, size_t> GetOrdinalHistogram(
+      const std::vector<proto::PlaintextToken>& tokens);
+
+  absl::Status VerifyEquivalentOrdinalCounts(
+      const std::vector<proto::PlaintextToken>& tokens);
+
+  absl::Status VerifyRevealRate(absl::Span<const proto::PlaintextToken> tokens,
+                                float p_reveal);
+
+ private:
+  explicit Verifier(std::unique_ptr<Decrypter> decrypter,
+                    std::unique_ptr<PlaintextTokenValidator> validator)
+      : validator_(std::move(validator)), decrypter_(std::move(decrypter)) {}
+
+  private_join_and_compute::ElGamalSecretKey elgamal_secret_key_;
+  std::unique_ptr<PlaintextTokenValidator> validator_;
+  std::unique_ptr<Decrypter> decrypter_;
+  constexpr static float kRevealRateTolerance = 0.001;  // 0.1% tolerance.
+};
+}  // namespace prtoken
+
+#endif  // PRTOKEN_VERIFIER_H_
